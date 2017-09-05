8d5bfb31dd4841cc81b9071c154b82d7a25ae170
Jonathan Lisic <jlisic@gmail.com>
2017-09-04 22:39:22 -0400

    registering so/dlls

---
+++ src/init.c
@@ -0,0 +1,44 @@
+/* Copyright (c) 2015-2016  Jonathan Lisic 
+ * Last edit: 16/08/24 - 09:20:44
+ * License: GPL (>=2) 
+ */  
+
+#include <stdio.h> 
+#include <stdlib.h>
+//#include <time.h>
+
+#include "R.h"
+#include "Rinternals.h"
+#include "Rmath.h"
+#include "kdtree.h"
+#include "meanShift.h"
+#if defined _OPENMP
+  #include <omp.h>
+#endif
+
+/***********************************/
+/* Register SO's                   */
+
+static R_NativePrimitiveArgType R_meanShift_t[] = {
+      REALSXP, REALSXP, INTSXP, INTSXP, INTSXP, INTSXP, INTSXP, INTSXP, 
+      REALSXP, REALSXP, REALSXP, REALSXP, INTSXP, INTSXP
+};
+
+static R_NativePrimitiveArgType R_knn_t[] = {
+      REALSXP, REALSXP, INTSXP, INTSXP, INTSXP, REALSXP, INTSXP, INTSXP, 
+      REALSXP, INTSXP, REALSXP
+};
+
+static const R_CMethodDef cMethods[] = {
+     {"R_meanShift", (DL_FUNC) &R_meanShift, 14, R_meanShift_t},
+     {"R_knn", (DL_FUNC) &R_knn, 14, R_knn_t},
+        {NULL, NULL, 0, NULL}
+};
+
+void R_init_myLib(DllInfo *info)
+{
+     R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+     R_useDynamicSymbols(info, TRUE); 
+}
+
+
---
+++ src/meanShift.h
@@ -0,0 +1,171 @@
+/* Copyright (c) 2015-2016  Jonathan Lisic 
+ * Last edit: 16/08/24 - 09:20:44
+ * License: GPL (>=2) 
+ */  
+
+#ifndef MEANSHIFT_HEADER
+
+#define MEANSHIFT_HEADER
+
+#include <stdio.h> 
+#include <stdlib.h>
+//#include <time.h>
+
+#include "R.h"
+#include "Rinternals.h"
+#include "Rmath.h"
+#include "kdtree.h"
+#include "meanshift.h"
+
+#if defined _OPENMP
+  #include <omp.h>
+#endif
+
+/***********************************************************************************************/
+/* print matrix                                                                                */
+/* NOTE: printing is done in column major form                                                 */
+/***********************************************************************************************/
+
+/* function that prints out a matrix of type integer */
+void printMatrixFullInt(int * X , size_t row, size_t col ); 
+
+
+/* function that prints out a matrix of type double */
+void printMatrixFullDbl(double * X , size_t row, size_t col ) ;
+
+
+/* function that prints out a matrix of type integer */
+void printMatrixFullSize_t(size_t * X , size_t row, size_t col );
+
+
+/***********************************************************************************************/
+/* this is an alternative kernel, with pre computed distances                                  */
+/***********************************************************************************************/
+/*
+ * query      - query matrix    queryRow by queryCol
+ * train      - train matrix    trainRow by queryCol
+ * neighbors  - neighbors       nNeighbors by queryRow
+ * queryRow   - rows for query
+ * trainRow   - rows for train
+ * queryCol   - columns for query and train
+ * nNeighbors - number of neighbors
+ * bandwidth  - bandwidth parameter
+ * alpha      - alpha parameter
+ */
+void normalKernelNewton_preDist(
+  double * query,        /* query matrix */
+  double * train,        /* train matrix */
+  size_t * neighbors,      /* neighbors matrix */
+  size_t queryRow,
+  size_t trainRow,
+  size_t queryCol,
+  size_t nNeighbors,
+  double * bandwidth2,    /* the squared reciprical of the bandwidth */
+  double * distance,
+  double alpha
+  );
+
+
+
+/***********************************************************************************************/
+/* this is a normal kernel                                                               */
+/***********************************************************************************************/
+/*
+ * query      - query matrix    queryRow by queryCol
+ * train      - train matrix    trainRow by queryCol
+ * neighbors  - neighbors       nNeighbors by queryRow
+ * queryRow   - rows for query
+ * trainRow   - rows for train
+ * queryCol   - columns for query and train
+ * nNeighbors - number of neighbors
+ * bandwidth  - bandwidth parameter
+ * alpha      - alpha parameter
+ */
+void normalKernelNewton(
+  double * query,        /* query matrix */
+  double * train,        /* train matrix */
+  size_t * neighbors,      /* neighbors matrix */
+  size_t queryRow,
+  size_t trainRow,
+  size_t queryCol,
+  size_t nNeighbors,
+  double * bandwidth2,    /* the squared reciprical of the bandwidth */
+  double alpha
+  );
+
+
+/***********************************************************************************************/
+/* this is a Epanechnikov product kernel                                                       */
+/***********************************************************************************************/
+/*
+ * query      - query matrix    queryRow by queryCol
+ * train      - train matrix    trainRow by queryCol
+ * neighbors  - neighbors       nNeighbors by queryRow
+ * queryRow   - rows for query
+ * trainRow   - rows for train
+ * queryCol   - columns for query and train
+ * nNeighbors - number of neighbors
+ * bandwidth  - bandwidth parameter
+ * alpha      - alpha parameter
+ */
+void epanechnikov_Kernel(
+  double * query,        /* query matrix */
+  double * train,        /* train matrix */
+  size_t * neighbors,      /* neighbors matrix */
+  size_t queryRow,
+  size_t trainRow,
+  size_t queryCol,
+  size_t nNeighbors,
+  double * bandwidth    /* the squared reciprical of the bandwidth */
+  );
+
+
+
+/***********************************************************************************************/
+/* this is a biweight product kernel                                                            */
+/***********************************************************************************************/
+/*
+ * query      - query matrix    queryRow by queryCol
+ * train      - train matrix    trainRow by queryCol
+ * neighbors  - neighbors       nNeighbors by queryRow
+ * queryRow   - rows for query
+ * trainRow   - rows for train
+ * queryCol   - columns for query and train
+ * nNeighbors - number of neighbors
+ * bandwidth  - bandwidth parameter
+ * alpha      - alpha parameter
+ */
+void biweight_Kernel(
+  double * query,        /* query matrix */
+  double * train,        /* train matrix */
+  size_t * neighbors,      /* neighbors matrix */
+  size_t queryRow,
+  size_t trainRow,
+  size_t queryCol,
+  size_t nNeighbors,
+  double * bandwidth    /* the squared reciprical of the bandwidth */
+  );
+
+
+/* mean shfit nearest neighbors */
+void R_meanShift(
+  double * query,                /* data to query for, in row major form */
+  double * train,                /* reference data, in row major form */
+  int * assignment,              /* assignment */
+  int * queryRowPtr,             /* number of rows of query data */
+  int * trainRowPtr,             /* number of rows of reference data */
+  int * queryColPtr,             /* number of columns of query and reference*/
+  int * nNeighborsPtr,           /* number of Neighbors */
+  int * iterationsPtr,           /* number of iterations */
+  double * bandwidth,    
+  double * alphaPtr,             /* ms to newton ajustment parameter */
+  double * epsilonPtr,           /* not used */
+  double * epsilonClusterPtr,           /* not used */
+  int * kernelEnumPtr,           /* not used */
+  int * algorithmEnumPtr,        /* not used */
+  int * intParameters,
+  int * dblParameters
+  );
+
+
+#endif
