31efb916bc4c1b5ebe2fc8fc816ee99ef2ca00c7
Jonathan Lisic <jlisic@gmail.com>
2019-01-21 10:10:21 -0500

    added reduced char/factor2

---
+++ tests/test-knn2.R
@@ -0,0 +1,46 @@
+# compare FNN to meanShiftR using exact nearest neighbors
+
+library(meanShiftR)
+
+i <- 1698
+
+set.seed(i)
+
+n <- 10 
+p <- 3 
+k <- 3 
+
+type <- c(1,1,1,2)
+x <- matrix( c( rnorm(p*n) , sample(1:3,size=n,replace=TRUE)), nrow=n)
+
+a <- proc.time()
+result_ms <- knn_meanShift(x,x,k=k, type=type)
+print( (proc.time() - a)[3])
+
+
+#calc dist
+
+mixed_dist <- function( x, y, type ) { 
+  sum((x[type == 1] - y[type == 1])^2) +
+  sum((x[type != 1] != y[type != 1])) 
+}
+
+Dist <- matrix(0,n,n)
+for( i in 1:n ) {
+  for( j in 1:n ) {
+    Dist[i,j] <- mixed_dist(x[i,], x[j,], type)
+  }
+}
+
+Dist_rank <- t(apply(Dist,1,order))[,3:1]
+
+print("Sum of rank differences:")
+print(sum(abs(Dist_rank - result_ms$neighbors)))
+
+Dist_k <- t(apply(Dist,1,sort))[,3:1]
+
+print("Max abs difference in distance: ")
+print(max(abs(Dist_k - result_ms$distance)))
+
+
+
